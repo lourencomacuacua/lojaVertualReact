dfc934d96f847e49b7d3f56e42d14b21a5938bf8
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-06 16:41:49 -0800

    rotas com parâmetros

--- src/Routess.tsx
+++ src/Routess.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Catalog from './pages/Catalog';
 import Admin from './pages/Admin';
+import ProductDetails from './pages/ProductDetails';
 
 const Routess = () => {
   return (
@@ -11,6 +12,7 @@ const Routess = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Catalog />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
