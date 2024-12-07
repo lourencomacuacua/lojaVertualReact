1d6f7a8f42449a32753272aaf02dcb9e96a0334e
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-07 10:19:46 -0800

    loader

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -8,8 +8,10 @@ import axios from 'axios';
 import { AxiosParams } from '../../types/vendor/axios';
 import { BASE_URL } from '../../util/requests';
 import { Link } from 'react-router-dom';
+import CardLoader from './CardLoader';
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params: AxiosParams = {
@@ -20,9 +22,14 @@ const Catalog = () => {
         size: 12,
       },
     };
-    axios(params).then((response) => {
-      setPage(response.data);
-    });
+    setIsLoading(true);
+    axios(params)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
   return (
     <div className="container my-4 catalog-container">
@@ -31,15 +38,19 @@ const Catalog = () => {
       </div>
 
       <div className="row">
-        {page?.content.map((product) => {
-          return (
-            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
-              <Link to="/product/1">
-                <ProductCard product={product} />
-              </Link>
-            </div>
-          );
-        })}
+        {isLoading ? (
+          <CardLoader />
+        ) : (
+          page?.content.map((product) => {
+            return (
+              <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+                <Link to="/product/1">
+                  <ProductCard product={product} />
+                </Link>
+              </div>
+            );
+          })
+        )}
       </div>
 
       <div className="row">
