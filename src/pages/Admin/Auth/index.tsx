015ce0a9e2c994ea9e1951ec002fea969377b195
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-07 12:47:45 -0800

    login responsivo estilizado

--- src/pages/Admin/Auth/index.tsx
+++ src/pages/Admin/Auth/index.tsx
@@ -5,7 +5,10 @@ const Auth = () => {
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
-        <h1>Divulgue seus produtos no DS Catalog</h1>
+        <h1>
+          Divulgue seus produtos <br />
+          no DS Catalog
+        </h1>
         <p>
           Faça parte de nosso catálogo de divulgação e aumente a venda dos seus
           produtos
