c83e46b9eeaebf9cb19f68de81ba5021a2d9cada
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-09 04:01:41 -0800

    layout do crud de produtos

--- src/pages/Admin/Products/List/index.tsx
+++ src/pages/Admin/Products/List/index.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
 const List = () => {
   return (
-    <div>
-      <button className="btn btn-primary text-white"> ADICIONAR</button>
-      <div>Search bar</div>
-    </div>
+    <>
+      <div className="product-crud-bar-container">
+        <Link to="/admin/products/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+
+        <div className="base-card product-filter-container">Search bar</div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-md-12"></div>
+      </div>
+    </>
   );
 };
 export default List;
