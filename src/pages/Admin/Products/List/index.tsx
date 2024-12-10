bdc1f9584bc6502eab230096c52634fe5bda305e
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-09 16:28:30 -0800

    productCrudCard horizontal responsiveness

--- src/pages/Admin/Products/List/index.tsx
+++ src/pages/Admin/Products/List/index.tsx
@@ -1,8 +1,29 @@
 import { Link } from 'react-router-dom';
 import './styles.css';
+import ProductCrudCard from '../ProductCrudCard';
+import { useEffect, useState } from 'react';
+import { SpringPage } from '../../../../types/vendor/spring';
+import { Product } from '../../../../types/product';
+import { requestBackend } from '../../../../util/requests';
+import { AxiosRequestConfig } from 'axios';
 const List = () => {
+  const [page, setPage] = useState<SpringPage<Product>>();
+  useEffect(() => {
+    const confing: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+    requestBackend(confing).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
+
   return (
-    <>
+    <div className="product-crud-container">
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-white btn-crud-add">
@@ -13,9 +34,13 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12"></div>
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 export default List;
