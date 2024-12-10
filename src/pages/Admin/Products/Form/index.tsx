24c42f4628118b26bbcc71a3f8dca863d0be3b15
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2024-12-09 18:12:13 -0800

    estilização dos botões do formulário

--- src/pages/Admin/Products/Form/index.tsx
+++ src/pages/Admin/Products/Form/index.tsx
@@ -1,4 +1,44 @@
+import './styles.css';
 const Form = () => {
-  return <h1>Tela form</h1>;
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">Dados do produto</h1>
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-botton-30 ">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  rows={8}
+                  className="form-control base-input h-auto"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Form;
