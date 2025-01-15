6745a01977c0393187267d77a53282acdce3fea8
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2025-01-15 06:24:52 -0800

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import 'react-toastify/dist/ReactToastify.css';
-import './assets/styles/custom.scss';
 import './App.css';
-import Routess from './Routess';
-import { useState } from 'react';
-import { AuthContext, AuthContextData } from './AuthContext';
-import { ToastContainer } from 'react-toastify';
-const App = () => {
-  const [authContextData, setAuthContextData] = useState<AuthContextData>({
-    authenticated: false,
-  });
 
+import { useState } from 'react';
+import Header from './components/header';
+
+const App = () => {
   return (
-    <AuthContext.Provider value={{ authContextData, setAuthContextData }}>
-      <Routess />
-      <ToastContainer />
-    </AuthContext.Provider>
+    <>
+      <Header />
+    </>
   );
 };
 
