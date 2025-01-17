34fe309b27bab824cb421c5ce05f2e475adc4c1b
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2025-01-17 07:30:40 -0800

    step 2 styling card

--- src/components/sales-summary/sales-sommary-card.tsx
+++ src/components/sales-summary/sales-sommary-card.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
 function SalesSommaryCard() {
-  return <div className="sales-sommary-card base-card"></div>;
+  return (
+    <div className="sales-sommary-card base-card">
+      <AvatarIcon />
+      <h3 className="sales-sommary-card-value">534.00</h3>
+      <span className="sales-sommary-card-label">Média</span>
+    </div>
+  );
 }
 
 export default SalesSommaryCard;
