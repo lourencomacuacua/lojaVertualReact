bd17b47fafa1bbda7bf1b500f79cdde42274bd50
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2025-01-17 08:52:01 -0800

    end styling donut chart

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/header';
 import Filter from './components/filter';
 import SalesByDate from './components/sales-by-dates';
 import SalesSummary from './components/sales-summary';
+import PieChartCard from './components/pie-chart-card';
 
 const App = () => {
   return (
@@ -15,6 +16,16 @@ const App = () => {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            name="Lojas"
+            labels={['Matola', 'Maputo', 'Inhambane']}
+            series={[40, 30, 30]}
+          />
+          <PieChartCard
+            name="Pagamentos"
+            labels={['Crédito', 'Débito', 'Pagamento']}
+            series={[20, 50, 30]}
+          />
         </div>
       </div>
     </>
